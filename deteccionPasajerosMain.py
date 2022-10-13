49aae55a6c770c9f7069dd84731c01d5e3ca5eb1
JHernandez99 <jl.hernandezcamacho@ugto.mx>
2022-10-12 23:27:28 -0500

    Deteccion de pasajeros minimap created

--- deteccionPasajerosMain.py
+++ deteccionPasajerosMain.py
@@ -10,25 +10,33 @@ pasajeros = 0
 while(video.isOpened()):
     ret, frame = video.read()
     if(ret == True):
+
         #============================================================
         result = frame.copy()
         imgSinRojo = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         lower = np.array([0, 100, 100])
         upper = np.array([1, 255, 255])
         mask = cv2.inRange(imgSinRojo, lower, upper)
-        cv2.imshow('mask', mask)
-
-
+        #cv2.imshow('mask', mask)
         imgGris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #imagen en gris
-        result = imgGris - mask
+        #cv2.imshow("gris",imgGris)
+        result = imgGris + mask
 
+
+        mpdr = 2 #multiplicador para la imagen de mapa
+        #print(result.shape)
+        result= result[150:238,80:280]
+        mapa2D = np.zeros((88*mpdr,200*mpdr,3),dtype=np.uint8)
+        mapa2D[:,:,:] = (255,255,255)
 
         pasajeros += len((face_cascade.detectMultiScale(result, 1.1, 5)))
         # print("Number of {0} faces!".format(len(faces)))
-        cv2.putText(frame, "Number of {0} faces!".format(pasajeros),
+        cv2.putText(frame, "Number of {0} passengers!".format(pasajeros),
                    org, font, 1, color, 2)
+        frame = cv2.rectangle(frame,(80,150),(280,238), (0,0,255),3)
         cv2.imshow("result", result)
         cv2.imshow("BUS", frame)
+        cv2.imshow("MAPA2D", mapa2D)
 
 
 
@@ -37,5 +45,6 @@ while(video.isOpened()):
             break
     else:
         break
+print("Pasajeros:{}".format(pasajeros))
 video.release()
 cv2.destroyAllWindows()

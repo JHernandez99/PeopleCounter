409d52f4034f7a4b07a541104cfc4ff8b7af2330
JHernandez99 <jl.hernandezcamacho@ugto.mx>
2022-10-20 12:00:16 -0500

    TRACKER CODE ADDED

--- deteccionPasajerosConSegmentacion.py
+++ deteccionPasajerosConSegmentacion.py
@@ -3,20 +3,25 @@ DETECCUION Y CONTEO DE PERSONAS QUE ABORDAN UN AUTOBUS
 SE EMPLEA METODO DE SEGMENTACION PARA REALIZAR LA DETECCION
 '''
 import cv2, numpy
+from tracker import *
 
 class Segmentacion:
     def __init__(self,no_video):
+        print("starting")
         self.no_video = no_video
         if self.no_video == 1:
-            pass
+            self.video = cv2.VideoCapture('videos/2015_05_12_10_49_54FrontColor.avi', )  # gente normal
         elif self.no_video == 2:
-            pass
+            self.video = cv2.VideoCapture('videos/2015_05_12_13_29_08FrontColor.avi',)#sombrero
         elif self.no_video == 3:
-            pass
+            self.video = cv2.VideoCapture('videos/2015_05_12_15_33_24FrontColor.avi',) #senior pelon
+        else:
+            self.video = cv2.VideoCapture('videos/2015_05_12_20_05_32FrontColor.avi',) #noche
+        print("done")
     def videoSegmentar(self):
-
         pass
     def tracking(self):
         pass
 
 sg = Segmentacion(1)
+

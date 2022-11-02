0031d4c7f2a7f80a018213df9113c92434e825c4
JHernandez99 <jl.hernandezcamacho@ugto.mx>
2022-11-02 09:11:04 -0600

    Preparing for use YOLO and train classifier for detect heads with the videos that I have

--- deteccionPasajerosConRed.py
+++ deteccionPasajerosConRed.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+from datetime import date
 import numpy as np
 from tracker import *
 
@@ -19,23 +21,24 @@ https://pysource.com/2021/12/07/how-to-count-people-from-cctv-cameras-with-openc
 '''
 
 class Segmentacion:
-    def __init__(self,no_video):
+    def __init__(self,no_video, flag):
         print("starting")
         try:
             self.no_video = no_video
-            if self.no_video == 1:
+            if self.no_video == 1 and flag == 1:
                 self.video = cv2.VideoCapture('videos/2015_05_12_10_49_54FrontColor.avi', )  # gente normal
-            elif self.no_video == 2:
+            elif self.no_video == 2 and flag == 1:
                 self.video = cv2.VideoCapture('videos/2015_05_12_13_29_08FrontColor.avi',)#sombrero
-            elif self.no_video == 3:
+            elif self.no_video == 3 and flag == 1:
                 self.video = cv2.VideoCapture('videos/2015_05_12_15_33_24FrontColor.avi',) #senior pelon
-            else:
-                self.video = cv2.VideoCapture('videos/2015_05_12_20_05_32FrontColor.avi',) #noche
+
         except:
             print("error leyendo video")
             raise SystemExit
         print("done")
-    def videoSegmentar(self):
+
+
+    def peopleCounter(self):
 
 
         while(self.video.isOpened()):
@@ -64,13 +67,43 @@ class Segmentacion:
                 cv2.destroyAllWindows()
                 break
 
+    def videoToImg4lblimg(self,rutaVideo):
+        self.video = cv2.VideoCapture('videos/'+rutaVideo, )  #ruta del video para extraer frames
+        contador = 0
+        num =0
+        while(self.video.isOpened()):
+            ret, frame = self.video.read()
+            if (ret == True):
+                contador+=1
+                if contador == 40:#guardar frame como imagen
+                    num+=1
+                    filename = 'images/{}Imagen{}.jpg'.format(rutaVideo,num)
+                    print(filename)
+                    status = cv2.imwrite(filename,frame)
 
+                    contador=0
+                    #cv2.imshow('frame', frame)
+                    if status:
+                        print('GUARDADO')
+                if (cv2.waitKey(30) == ord('s')):
+                    break
+            else:
+                cv2.destroyAllWindows()
+                break
 
 
 
 
     def tracking(self):
         pass
-for i in range(0,5):
-    sg = Segmentacion(1)
-    sg.videoSegmentar()
+
+
+
+sg = Segmentacion(0,0)
+#sg.videoToImg4lblimg()
+carpetaVideos = os.listdir('videos')
+print(len(carpetaVideos))
+for rutaVideo in carpetaVideos:
+    print(rutaVideo)
+    #sg.peopleCounter()
+    sg.videoToImg4lblimg(rutaVideo)
